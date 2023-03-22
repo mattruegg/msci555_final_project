f6bfac7756f6528558fc04c843b676cfaea12524
Matt Ruegg <matt.ruegg9@gmail.com>
2023-03-21 22:36:26 -0400

    adding and reading artist data

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import pandas as pd
+
 if __name__ == '__main__':
-    print("hello world")
+    artist_data = pd.read_csv('artist_data.csv')
+
+    for x in range(len(artist_data)):
+        print(artist_data.loc[x, 'Name'])
